cbb4681f605e93661ba0bb241e6422b6a3d058a1
KimDonghyeok <qoophon@gmail.com>
2024-04-11 23:22:51 +0900

    modify: PhotoDetailScreen 화면 수정

--- lib/presentation/photo_detail/photo_detail_screen.dart
+++ lib/presentation/photo_detail/photo_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_image_search_with_provider/data/model/photo.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PhotoDetailScreen extends StatelessWidget {
   final Photo photo;
@@ -9,22 +10,40 @@ class PhotoDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('이미지 상세'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Hero(
-                  tag: photo.id,
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.network(photo.url))),
+      appBar: AppBar(
+        title: Text('이미지 상세'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Hero(
+                tag: photo.id,
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      photo.url,
+                      fit: BoxFit.cover,
+                    ))),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: photo.tags
+                  .split(', ')
+                  .toList()
+                  .map((tag) => Text(
+                        tag,
+                        style: GoogleFonts.merienda(
+                            textStyle: const TextStyle(fontSize: 24)),
+                      ))
+                  .toList(),
             ),
-          ],
-        ));
+          )
+        ],
+      ),
+    );
   }
 }
